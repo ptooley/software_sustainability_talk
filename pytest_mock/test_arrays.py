58ff11b3e2781fae304d25a3e982974eec82b295
Phil Tooley <phil.tooley@sheffield.ac.uk>
2018-09-19 20:34:58 +0100

    initial commit

---
+++ pytest_mock/test_arrays.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import pytest
+
+
+
+def test_build_array():
+    pass
+
+def test_copy_array():
+    pass
+
+def test_multiply_array():
+    raise ValueError("Wrong dimensions in array")
+
